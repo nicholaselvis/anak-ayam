0a228442df193380577451dc57ac6a0e698b7a4f
nchlslvs <92220344+nicholaselvis@users.noreply.github.com>
2021-12-12 21:48:42 +0800

    Add files via upload

---
+++ anak ayam.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int n,a;
+	
+	cout<<"Anak ayam turunlah ";
+	cin>>n;
+	
+	for(int a; a < n-1; n--){
+		cout<<"Mati satu tiggallah ";
+		cout<<n-1<<endl;
+	}
+	
+	cout<<"Mati satu tinggal induknya";
+	
+}
